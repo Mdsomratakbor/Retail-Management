f599f41eee99820851a21c8786c075017019de3d
Mdsomratakbor <somrat876@gmail.com>
2020-08-17 22:16:52 +0600

    add WPF project

---
+++ RMDekstopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using RMDekstopUI.ViewModels;
+using RMDekstopUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDekstopUI
+{
+    public class Bootstrapper :BootstrapperBase
+    {
+        public Bootstrapper() {
+
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShallViewModel>();
+        }
+    }
+}
