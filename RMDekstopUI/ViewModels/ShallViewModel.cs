4e3f8993c66832379db012b5eb4d237091abdbbd
Mdsomratakbor <somrat876@gmail.com>
2020-08-17 22:05:31 +0600

    add MVVM pattern

---
+++ RMDekstopUI/ViewModels/ShallViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDekstopUI.ViewModels
+{
+    public class ShallViewModel
+    {
+    }
+}
