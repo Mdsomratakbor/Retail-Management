f4b2020807cf3999126f96ae0ce2b180dd319bed
Mdsomratakbor <somrat876@gmail.com>
2020-08-19 22:27:41 +0600

    add login form

--- RMDekstopUI/ViewModels/LoginViewModel.cs
+++ RMDekstopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace RMDekstopUI.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
+        private string _userName;
+        private string _password;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+        
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+        public bool CanLogIn(string usrName, string password)
+        {
+            bool output = false;
+            if(usrName.Length> 0 && password.Length > 0)
+            {
+                output = true;
+            }
+            return output;
+        
+        }    
+        public void LogIn(string userName, string password)
+        {
+            Console.WriteLine();
+        }
+   
     }
 }
