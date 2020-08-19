cbf7708c5cd6ab49d11abbc7c2e58eec30bc044d
Mdsomratakbor <somrat876@gmail.com>
2020-08-19 21:10:38 +0600

    Add LoginViewModel class and LogginView userConntrol WPF

--- RMDekstopUI/ViewModels/LoginViewModel.cs
+++ RMDekstopUI/ViewModels/LoginViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Caliburn.Micro;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RMDekstopUI.ViewModels
 {
-    public class ShallViewModel: Conductor<Object>
+    public class LoginViewModel
     {
     }
 }
