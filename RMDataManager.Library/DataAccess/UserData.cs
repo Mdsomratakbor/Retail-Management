6fd2d799b621ab990892a6902cc31c7c8aec6bc1
Mdsomratakbor <somrat876@gmail.com>
2020-08-21 23:33:28 +0600

    add UserController

--- RMDataManager.Library/DataAccess/UserData.cs
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,10 @@ namespace RMDataManager.Library.DataAccess
     {
         public List<UserModel> GetUserById(string Id)
         {
-
+            SqlDataAccess sql = new SqlDataAccess();
+            var p = new { Id = Id };
+           var output =  sql.LoadData<UserModel, dynamic>("dbo.spUserLookUp", p, "DefaultConnection");
+            return output;
         }
     }
 }
---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+
+        // GET: User/GetbyId/5
+        public List<UserModel> GetbyId(string id)
+        {
+            UserData data = new UserData();
+            data.GetUserById(id);
+            return View();
+        }
+        
+    }
+}
