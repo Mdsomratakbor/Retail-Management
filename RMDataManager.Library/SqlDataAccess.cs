862a9c80b1c3814fb03fb130155638fd5588c471
Mdsomratakbor <somrat876@gmail.com>
2020-08-21 23:10:36 +0600

    add two method for data saving and data load

--- RMDataManager.Library/SqlDataAccess.cs
+++ RMDataManager.Library/SqlDataAccess.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,31 @@ namespace RMDataManager.Library
 {
     public class SqlDataAccess
     {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storeProcedure, U parametars, string connectionStringName)
+        {
+            string connection = GetConnectionString(connectionStringName);
+            using (IDbConnection cnn = new SqlConnection(connection))
+            {
+                List<T> rows = cnn.Query<T>(storeProcedure, parametars, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
+        }
+        public void SaveData<T>(string storeProcedure, T parametars, string connectionStringName)
+        {
+            string connection = GetConnectionString(connectionStringName);
+            using (IDbConnection cnn = new SqlConnection(connection))
+            {
+                 cnn.Execute(storeProcedure, parametars, commandType: CommandType.StoredProcedure);
+              
+            }
+        }
+
+
+
     }
 }
