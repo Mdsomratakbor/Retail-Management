d82f6979022f58d55b9cfda847536e350ae14db5
Mdsomratakbor <somrat876@gmail.com>
2021-02-21 19:12:25 +0600

    all table relationship is complete

--- RMData/dbo/Tables/Inventory.sql
+++ RMData/dbo/Tables/Inventory.sql
@@ -4,5 +4,6 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NULL DEFAULT 0, 
-    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GetDate()
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GetDate(), 
+    CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY (ProductId) REFERENCES Product(Id)
 )
