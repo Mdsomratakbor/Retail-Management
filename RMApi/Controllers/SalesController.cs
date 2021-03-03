eb9e633caa02406cf75cf75789c155947b7bdaea
Mdsomratakbor <somrat876@gmail.com>
2021-03-03 23:44:06 +0600

    Added New API and Moved Over Functionality

---
+++ RMApi/Controllers/SalesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SalesController : ControllerBase
+    {
+        [Authorize(Roles = "Casheir")]
+        public void Post(SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            data.SaveSale(sale, userId);
+        }
+        [Authorize(Roles = "Admin, Manager")]
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            SaleData data = new SaleData();
+            return data.GetSaleReport();
+        }
+    }
+}
